e5352ceb2e40be6486bacf0fec95fd89794f78e0
Dennis Won <jhwon0820@gmail.com>
2024-06-12 09:43:45 -0700

    feat: comprehensive rust 26

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+mod widget;
+
+use widget::Button;
+use widget::Label;
+use widget::Widget;
+use widget::Window;
+
 fn main() {
-    println!("Hello, world!");
+  let mut window = Window::new("Rust GUI Demo 1.23");
+  window.add_widget(Box::new(Label::new("This is a small text GUI demo.")));
+  window.add_widget(Box::new(Button::new("Click me!")));
+  window.draw();
 }
